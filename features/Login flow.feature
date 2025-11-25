527f9262ba1140e7e9841701dd376074c14985ff
yogesh.singh@blinq.io <Yogesh Singh>
2025-11-25 07:33:51 +0000

    Yogesh Singh pushed changes to main branch from recorder

--- features/Login flow.feature
+++ features/Login flow.feature
@@ -1,5 +1,7 @@
 Feature: Login flow
 
   Scenario: Enter password
-    # Click on Password
+    # Click on Password textbox
     Given The user navigates to the login page and clicks on the Password textbox
+    # Click on Accepted usernames are:
+    When The user clicks on the paragraph Accepted usernames are:
--- features/step_definitions/login_page.mjs
+++ features/step_definitions/login_page.mjs
@@ -10,6 +10,17 @@ const elements = {
     element_name: "Password textbox",
     element_key: "textbox_password",
   },
+  paragraph_accepted_usernames_are_: {
+    locators: [
+      { css: 'internal:text="Accepted usernames are:"s', priority: 1 },
+      { css: 'internal:text="Accepted usernames are:"i', priority: 1 },
+      { css: 'p >> internal:has-text="Accepted usernames are:"i', priority: 1 },
+      { css: "p >> internal:has-text=/^Accepted usernames are:$/", priority: 1 },
+      { css: 'internal:role=paragraph >> internal:has-text="Accepted usernames are:"i', priority: 1 },
+    ],
+    element_name: "Accepted usernames are: paragraph",
+    element_key: "paragraph_accepted_usernames_are_",
+  },
 };
 
 /**
@@ -29,4 +40,23 @@ Given(
   "The user navigates to the login page and clicks on the Password textbox",
   { timeout: 60000 },
   the_user_navigates_to_the_login_page_and_clicks_on_the_password_textbox
+);
+
+/**
+ * The user clicks on the paragraph Accepted usernames are:
+ * @recorder
+ * @path=/login
+ */
+async function the_user_clicks_on_the_paragraph_accepted_usernames_are() {
+  // source: recorder
+  // implemented_at: 2025-11-25T07:33:50.038Z
+  const _params = {};
+  // Click on Accepted usernames are: paragraph
+  await context.web.click(elements["paragraph_accepted_usernames_are_"], _params, null, this);
+}
+
+When(
+  "The user clicks on the paragraph Accepted usernames are:",
+  { timeout: 60000 },
+  the_user_clicks_on_the_paragraph_accepted_usernames_are
 );
